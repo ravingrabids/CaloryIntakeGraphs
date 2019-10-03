d8c1483ec6f3ada5a3f4b1ebc53ed3de0cade6c8
Andrei Goncharov <andreigoncharov021@gmail.com>
2019-10-04 00:07:24 +0300

    Initial Commit

---
+++ CaloryIntake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaloryIntake
+//
+//  Created by Андрей Гончаров on 04/10/2019.
+//  Copyright © 2019 Андрей Гончаров. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
